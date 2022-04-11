871a68698e917ea0bd97d5209719b461f1f3570f
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-04-11 19:20:44 -0400

    made methods for queues

--- queue.js
+++ queue.js
@@ -20,25 +20,62 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    let newNode = new Node(val)
 
+    if(this.first === null){
+      this.first = newNode;
+    }
+    else{
+      this.last.next = newNode;
+    }
+    this.last = newNode;
+    this.size += 1;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if(this.size === 0){
+      throw new Error("Empty queue");
+    }
+    
+    let currentNode = this.first;
 
+    //Queue has one element
+    if(this.size === 1){
+      this.first = null;
+      this.last = null;
+    }
+
+    //Any other case
+    else{
+      this.first = currentNode.next;
+    }
+    this.size -= 1;
+    return currentNode.val;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
+    if(this.size === 0){
+      throw new Error("Empty queue");
+    }
+
+    return this.first.val;
 
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
+    if(this.size === 0){
+      return true;
+    }
+    else{
+      return false;
+    }
 
   }
 }
