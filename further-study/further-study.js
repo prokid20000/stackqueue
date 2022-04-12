15947f16936f0c96019467e10057137cae3b2738
Kate Moser <komoser@gmail.com>
2022-04-11 19:16:41 -0500

    working on further study

--- further-study/further-study.js
+++ further-study/further-study.js
@@ -2,9 +2,28 @@ const Queue = require("../queue");
 const Stack = require("../stack");
 
 function stringReversal(str) {
+  let letterStack = new Stack();
+  for(let char of str){
+    letterStack.push(char);
+  }
+
+  let reverseStr = "";
+  while(!letterStack.isEmpty()){
+    reverseStr = reverseStr + letterStack.pop();
+  }
+  return reverseStr;
 }
 
 function balancedBrackets(str) {
+  let bracketStack = new Stack();
+  let openingBrackets = "{([";
+  for(let char of str){
+    if(openingBrackets.includes(char)){
+      bracketStack.push(char);
+    }
+    if(char === "]" && bracketStack.peek() === "["){
+    }
+  }
 }
 
 function josephusSurvivor(peopleNum, skip) {
